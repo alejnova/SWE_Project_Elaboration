6e623f506186e2f19a84f5877f97493797120e95
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2026-04-11 00:25:32 -0400

    added timer viewmodel

---
+++ ADHD_Software_Engineering/Controllers/TimerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ADHD_Software_Engineering.Models;
+
+namespace ADHD_Software_Engineering.Controllers
+{
+    public class TimerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new TimerViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Start(TimerViewModel model)
+        {
+            model.IsRunning = true;
+            model.Status = "Running";
+            model.RemainingSeconds = model.Duration * 60;
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult Pause(TimerViewModel model)
+        {
+            model.IsRunning = false;
+            model.Status = "Paused";
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult Reset(TimerViewModel model)
+        {
+            model.IsRunning = false;
+            model.Status = "Reset";
+            model.RemainingSeconds = model.Duration * 60;
+            return View("Index", model);
+        }
+    }
+}
---
+++ ADHD_Software_Engineering/Models/TimerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ADHD_Software_Engineering.Models
+{
+    public class TimerViewModel
+    {
+        public int Duration { get; set; } = 25;
+        public int RemainingSeconds { get; set; } = 25 * 60;
+        public bool IsRunning { get; set; } = false;
+        public string Status { get; set; } = "Ready";
+    }
+}
+
